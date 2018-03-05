32684a7f8d867500e94b1c685cfabb050b38122b
mbenfredj <benfredjmajdi@gmail.com>
2018-03-05 12:12:26 +0100

    Init project

---
+++ mvc_client_cat/src/main/java/com/mbenfredj/MvcClientCatApplication.java
@@ -0,0 +1,12 @@
+package com.mbenfredj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcClientCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcClientCatApplication.class, args);
+	}
+}
