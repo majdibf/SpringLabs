3597d6d69b2f3ae682c0be7aa5c87b6df07827ec
mbenfredj <benfredjmajdi@gmail.com>
2018-03-05 17:04:12 +0100

    Init de la database avec quelques enregistrements

--- mvc_client_cat/src/main/java/com/mbenfredj/MvcClientCatApplication.java
+++ mvc_client_cat/src/main/java/com/mbenfredj/MvcClientCatApplication.java
@@ -1,12 +1,26 @@
 package com.mbenfredj;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mbenfredj.dao.ProduitRepository;
+import com.mbenfredj.entities.Produit;
+
 @SpringBootApplication
-public class MvcClientCatApplication {
+public class MvcClientCatApplication implements CommandLineRunner {
+	@Autowired
+	private ProduitRepository produitRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcClientCatApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		produitRepository.save(new Produit("Samsung Galaxy S8", 513, 100));
+		produitRepository.save(new Produit("Xiaomi Mi A1", 173, 200));
+		produitRepository.save(new Produit("Huawei Mate 10 Pro", 662, 220));
 	}
 }
